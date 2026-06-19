12a05467fcb886c17b9bb029dc13dfc5189de0db
RudainaAly54 <rudainaaly154@gmail.com>
2026-06-19 17:09:01 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+<>
+<h1 className="text-sm font-bold underline text-[#ff6b6b]">
+      Hello world!
+    </h1>
+</>
+  )
+}
+
+export default App
