c8e5cb026798a2fbc12423eb1b2c2f1aa414ed3c
RudainaAly54 <rudainaaly154@gmail.com>
2026-06-22 01:24:12 +0300

    Done

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -128,7 +128,7 @@ const Projects = () => {
                             animate={{ opacity: 1, scale: 1, y: 0 }}
                             exit={{ opacity: 0, scale: 0.9, y: 40 }}
                             transition={{ duration: 0.3 }}
-                            className="fixed  top-10 left-5 md:left-[50%] lg:left-[50%]  z-50 w-[90%] max-w-2xl max-h-[85vh] overflow-y-auto bg-[rgba(10,6,25,0.97)] border border-[rgba(147,51,234,0.35)] rounded-2xl shadow-2xl shadow-purple-900/40 p-10 flex flex-col gap-6"
+                            className="fixed  top-10 left-5 md:left-[25%] lg:left-[35%]  z-50 w-[90%] max-w-2xl max-h-[85vh] overflow-y-auto bg-[rgba(10,6,25,0.97)] border border-[rgba(147,51,234,0.35)] rounded-2xl shadow-2xl shadow-purple-900/40 p-10 flex flex-col gap-6"
                         >
                             {/* Close button */}
                             <button
